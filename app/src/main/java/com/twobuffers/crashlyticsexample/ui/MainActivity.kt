8c93058ddc64d3cf88f58430d2896d6d5542301a
Greg Rynkowski <dev@rynkowski.info>
2019-11-13 15:26:18 +0000

    Extend a sample crashing test with logs
    app/src/main/java/com/twobuffers/crashlyticsexample/ui/MainActivity.kt

--- app/src/main/java/com/twobuffers/crashlyticsexample/ui/MainActivity.kt
+++ app/src/main/java/com/twobuffers/crashlyticsexample/ui/MainActivity.kt
@@ -18,14 +18,18 @@ package com.twobuffers.crashlyticsexample.ui
 
 import android.os.Bundle
 import com.crashlytics.android.Crashlytics
+import com.twobuffers.base.utils.Logger
 import com.twobuffers.crashlyticsexample.BuildConfig
 import com.twobuffers.crashlyticsexample.R
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 import kotlinx.android.synthetic.main.activity_main.build_flavour_txt
 import kotlinx.android.synthetic.main.activity_main.default_crash_btn
 import kotlinx.android.synthetic.main.activity_main.flavour_crash_btn
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +41,14 @@ class MainActivity : DaggerAppCompatActivity() {
         val flavour = BuildConfig.FLAVOR.capitalize()
         build_flavour_txt.text = "$flavour app"
         default_crash_btn.setOnClickListener {
+            logger.i("default_crash_btn clicked!")
             Crashlytics.getInstance().crash() // Force a crash
         }
         flavour_crash_btn.text = "$flavour crash!"
         flavour_crash_btn.setOnClickListener {
+            logger.i("flavour_crash_btn clicked!")
             throw RuntimeException("$flavour exception happened.")
         }
+        logger.i("Testing buttons ready.")
     }
 }
